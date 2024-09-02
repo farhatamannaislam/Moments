cc5820035119a224f86a2bd49729a0a2c66ef2e3
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-02 17:22:37 +0000

    Add: SignIn SignUp and authentication

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api-rc-fb80a7a94541.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+
+
+  
