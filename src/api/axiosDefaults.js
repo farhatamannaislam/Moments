61502a19b66496b0686387ec2e7fc88b78d6cff9
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-03 08:09:29 +0000

    Refactor:context hook

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,7 +3,3 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-rc-fb80a7a94541.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
-
-
-
-  
